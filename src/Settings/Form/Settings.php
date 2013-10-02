4304fd015065826aa121a103f7cb86e304e39722
Mathias Weitz <weitz@xenon>
2013-10-02 18:32:19 +0200

    2013.10.02

--- src/Settings/Form/Settings.php
+++ src/Settings/Form/Settings.php
@@ -2,83 +2,10 @@
 
 namespace Settings\Form;
 
-use Zend\ServiceManager\ServiceLocatorAwareInterface;
-use Zend\ServiceManager\ServiceLocatorInterface;
-use Core\Entity\Hydrator\EntityHydrator;
-use Zend\Form\Form;
-use Zend\Form\Element;
-use Zend\Stdlib\Hydrator\ClassMethods;
-use Settings\Entity\Settings as SettingsEntity;
-use Zend\Stdlib\Hydrator\ArraySerializable;
-
-class Settings extends Form implements ServiceLocatorAwareInterface {
+class Settings extends FormAbstract {
 	
-	protected $forms;
-
-	public function __construct($name = null) {
-		parent::__construct('settings');
-		$this->setAttribute('method', 'post');
-                $this->setBindOnValidate(Form::BIND_ON_VALIDATE);
-	
-		// 'action' => $this->url('lang/settings', array(), true),
-	}
-	
-        /**
-         * 
-         * @param \Zend\ServiceManager\ServiceLocatorInterface $serviceLocator
-         * @return \Settings\Form\Settings
-         */
-	public function setServiceLocator(ServiceLocatorInterface $serviceLocator)
-	{
-		$this->forms = $serviceLocator;
-		return $this;
-	}
-	
-	public function getServiceLocator()
-	{
-		return $this->forms;
-	}
-    
-    public function getHydrator() {
-        if (!$this->hydrator) {
-            $hydrator = new ArraySerializable();
-            $this->setHydrator($hydrator);
-        }
-        return $this->hydrator;
+    public function getCoreFieldset() {
+        return 'settings-core-fieldset';
     }
     
-    protected function getPlugin($name) {
-        $plugin = Null;
-        $factory = $this->getFormFactory();
-        $formElementManager = $factory->getFormElementManager();
-        if (isset($formElementManager)) {
-            $serviceLocator = $formElementManager->getServiceLocator();
-            $viewhelpermanager = $serviceLocator->get('viewhelpermanager');
-            if (isset($viewhelpermanager)) {
-                $plugin = $viewhelpermanager->get($name);
-            }
-        }
-        return $plugin;
-    }
-
-    public function init() {
-
-        $this->setName('setting-core');
-        
-        $plugin = $this->getPlugin('url');
-        $url = call_user_func_array($plugin, array(null, array('lang' => 'de')));
-        $this->setAttribute('action', $url);
-        
-        //->setHydrator(new ModelHydrator())
-        //->setObject(new SettingsEntity());
-        
-        $this->add(
-        		$this->forms->get('settings-core-fieldset')
-        		->setUseAsBaseFieldset(true)
-        );
-                
-        $this->add($this->forms->get('DefaultButtonsFieldset'));
-
-    }
-
 }
